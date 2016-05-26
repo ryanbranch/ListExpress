477c426734f2d1c2070fab8682f30d0a3ef07a0f
rjbranch <rjbrjb@umich.edu>
2016-05-26 10:16:59 -0400

    Begin Django rewrite
    Now that I have a bit more Django experience I wanted to give the app
    another shot.  But because I identified several issues with the way I
    was doing things before, I wanted to begin again with a clean slate.

---
+++ listsite/listexpress/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ListexpressConfig(AppConfig):
+    name = 'listexpress'
--- vote/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import ItemList
-
-class ItemListForm(forms.ModelForm):
-
-    class Meta:
-        model = ItemList
-        fields = ('title', 'list_text',)
